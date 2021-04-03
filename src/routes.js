809325c6b019d3b05d6e4e2b12452fe18accabca
Roberto Ribeiro <robert.58@hotmail.com>
2021-04-02 22:11:02 -0300

    aula 01 concluida

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const routes = express.Router()
+
+const views = __dirname + "/views/"
+
+const profile = {
+    name: "Roberto",
+    avatar: "https://avatars.githubusercontent.com/u/60557871?v=4",
+    "monthly-budget": 3000,
+    "days-per-week": 5,
+    "hours-per-day": 5,
+    "vacation-per-year": 4
+}
+
+//req,res request e response
+routes.get('/', (req, res) => res.render(views + "index"))
+routes.get('/job', (req, res) => res.render(views + "job"))
+routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
+routes.get('/profile', (req, res) => res.render(views + "profile", { profile }))
+
+
+module.exports = routes;
